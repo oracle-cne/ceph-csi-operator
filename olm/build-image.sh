ef3823c5d8745655a500027dbc9f6323f0464e57
Prasad Shirodkar <prasad.shirodkar@oracle.com>
2025-11-17 16:24:46 -0800

    updated tag for rebuild

--- olm/build-image.sh
+++ olm/build-image.sh
@@ -7,7 +7,7 @@ CONTAINER_CLI="${CONTAINER_CLI:-podman}"
 name="ceph-csi-operator"
 version="0.4.0"
 registry="container-registry.oracle.com/olcne"
-docker_tag=${registry}/${name}:v${version}
+docker_tag=${registry}/${name}:v${version}-1
 
 "${CONTAINER_CLI}" build --pull \
     --build-arg https_proxy=${https_proxy} \
