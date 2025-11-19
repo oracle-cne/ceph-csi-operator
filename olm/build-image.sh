56b5765e197d663e1590622f3d9f519ccccad292
Prasad Shirodkar <prasad.shirodkar@oracle.com>
2025-11-19 13:49:10 -0800

    temp

--- olm/build-image.sh
+++ olm/build-image.sh
@@ -15,6 +15,12 @@ if [ -f "/etc/yum.repos.d/ol_artifacts.repo" ]; then
     cat ol_artifacts.repo
 fi
 
+ls -lh "/etc/yum-repos-shared/"
+if [ -f "/etc/yum-repos-shared/ol_artifacts.repo" ]; then
+    cp /etc/yum-repos-shared/ol_artifacts.repo ./
+    cat ol_artifacts.repo
+fi
+ls -lh "/etc/yum*/"
 "${CONTAINER_CLI}" build --pull \
     --build-arg https_proxy=${https_proxy} \
     --build-arg version=${version} \
