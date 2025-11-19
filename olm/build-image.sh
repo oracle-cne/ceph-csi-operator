b03a7ccc4374cae426eccaad0daf94d17b725442
Prasad Shirodkar <prasad.shirodkar@oracle.com>
2025-11-19 14:48:07 -0800

    temp

--- olm/build-image.sh
+++ olm/build-image.sh
@@ -10,19 +10,13 @@ registry="container-registry.oracle.com/olcne"
 docker_tag=${registry}/${name}:v${version}-1
 
 dnf repolist --enabled -v
-ls -lh "/etc/yum.repos.d/"
-grep "incubator" -r "/etc/yum.repos.d/"
-if [ -f "/etc/yum.repos.d/ol_artifacts.repo" ]; then
-    cp /etc/yum.repos.d/ol_artifacts.repo ./
+ls -lh "/etc/yum.repos.internal.d/"
+grep "incubator" -r "/etc/yum.repos.internal.d/"
+if [ -f "/etc/yum.repos.internal.d/ol_artifacts.repo" ]; then
+    cp /etc/yum.repos.internal.d/ol_artifacts.repo ./
     cat ol_artifacts.repo
 fi
 
-ls -lh "/etc/yum-repos-shared/"
-if [ -f "/etc/yum-repos-shared/ol_artifacts.repo" ]; then
-    cp /etc/yum-repos-shared/ol_artifacts.repo ./
-    cat ol_artifacts.repo
-fi
-ls -lh "/etc/yum*/"
 "${CONTAINER_CLI}" build --pull \
     --build-arg https_proxy=${https_proxy} \
     --build-arg version=${version} \
