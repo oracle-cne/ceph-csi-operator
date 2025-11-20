bc13012f2d1818ca4e3699202bf974c3d22d48ea
Prasad Shirodkar <prasad.shirodkar@oracle.com>
2025-11-19 16:30:25 -0800

    build with golang from internal repos

--- olm/build-image.sh
+++ olm/build-image.sh
@@ -9,12 +9,8 @@ version="0.4.0"
 registry="container-registry.oracle.com/olcne"
 docker_tag=${registry}/${name}:v${version}-1
 
-dnf repolist --enabled -v
-ls -lh "/etc/yum.repos.internal.d/"
-grep "incubator" -r "/etc/yum.repos.internal.d/"
 if [ -f "/etc/yum.repos.internal.d/ol_artifacts.repo" ]; then
     cp /etc/yum.repos.internal.d/ol_artifacts.repo ./
-    cat ol_artifacts.repo
 fi
 
 "${CONTAINER_CLI}" build --pull \
