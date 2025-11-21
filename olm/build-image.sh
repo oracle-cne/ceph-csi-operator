762692fc86c72f26f156f38d558b3350b5dde554
Prasad Shirodkar <prasad.shirodkar@oracle.com>
2025-11-21 00:18:09 +0000

    updates

--- olm/build-image.sh
+++ olm/build-image.sh
@@ -9,11 +9,13 @@ version="0.4.0"
 registry="container-registry.oracle.com/olcne"
 docker_tag=${registry}/${name}:v${version}-1
 
-if [ -f "/etc/yum.repos.internal.d/ol_artifacts.repo" ]; then
-    cp /etc/yum.repos.internal.d/ol_artifacts.repo ./
-fi
+args=(--build-arg https_proxy=${https_proxy}
+	--build-arg version=${version})
 
-"${CONTAINER_CLI}" build --pull \
-    --build-arg https_proxy=${https_proxy} \
-    --build-arg version=${version} \
-    -t ${docker_tag} -f ./olm/builds/Dockerfile .
+[[ -f /etc/yum.repos.internal.d/ol_artifacts.repo ]] && \
+	args+=(--volume /etc/yum.repos.internal.d/ol_artifacts.repo:/etc/yum.repos.internal.d/ol_artifacts.repo --build-arg DNF_GOLANG_ARG="--setopt=ol9_appstream.exclude=golang")
+[[ -f /etc/yum.conf ]] && args+=(--volume /etc/yum.conf:/etc/yum.conf)
+
+args+=(-t ${docker_tag} -f ./olm/builds/Dockerfile .)
+
+"${CONTAINER_CLI}" build --pull "${args[@]}"
